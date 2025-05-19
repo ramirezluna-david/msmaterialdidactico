052a3b814a3c89d432bc954e6579479ffd40840f
David A. Ramírez Luna <davidrrluna@gmail.com>
2025-05-19 10:47:31 -0400

    first commit

---
+++ src/main/java/com/msmaterialdidactico/msmaterialdidactico/MsmaterialdidacticoApplication.java
@@ -0,0 +1,13 @@
+package com.msmaterialdidactico.msmaterialdidactico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsmaterialdidacticoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsmaterialdidacticoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/msmaterialdidactico/msmaterialdidactico/MsmaterialdidacticoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msmaterialdidactico.msmaterialdidactico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsmaterialdidacticoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
